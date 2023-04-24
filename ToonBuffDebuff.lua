25489e379d0c120da7a628a0af792133efcd16ad
GameDevMadeEasy <jglover88@hotmail.com>
2023-04-24 02:20:32 -0500

    Full working source.

---
+++ ToonBuffDebuff.lua
@@ -0,0 +1,151 @@
+local debuffFrame = createFrame(160, 32)
+local buffFrame = createFrame(350, 32)
+InitializePosition(buffFrame)
+InitializePosition(debuffFrame)
+
+-- Create the buff and debuff icons
+local BUFF_ICON_SIZE = 32
+local NUM_BUFFS = 20
+for i = 1, NUM_BUFFS do
+    local buffIcon = CreateSpellIcon(buffFrame, "")
+    buffIcon:SetPoint("TOPLEFT", buffFrame, "TOPLEFT", (i-1)*BUFF_ICON_SIZE, 0)
+end
+
+local DEBUFF_ICON_SIZE = 32
+local NUM_DEBUFFS = 20
+for i = 1, NUM_DEBUFFS do
+    local debuffIcon = CreateSpellIcon(debuffFrame, "")
+    debuffIcon:SetPoint("TOPLEFT", debuffFrame, "TOPLEFT", (i-1)*DEBUFF_ICON_SIZE, 0)
+end
+
+local existingBuffIDs = {}
+
+local function UpdateBuffDisplay()
+    -- Get the buff data with spell icons
+    local buffsData, _ = buffsDebuffs()
+
+    -- Handle buffs
+    local buffIndex = 1
+    for _, buffData in pairs(buffsData) do
+        local buffIcon = existingBuffIDs[buffData.spellId]
+        if not buffIcon then
+            -- Create a new buff icon frame
+            buffIcon = CreateSpellIcon(buffFrame, buffData.texture)
+            existingBuffIDs[buffData.spellId] = buffIcon
+        end
+
+        local buffText = ''
+        local buffExpirationTime = buffData.expirationTime - GetTime()
+        if buffExpirationTime <= 0 or buffExpirationTime >= 90 then
+            ResetInactiveBuffIcon(buffIcon) -- Call the new function to reset and hide inactive buff icons
+            existingBuffIDs[buffData.spellId] = nil
+        else
+            if buffData.modifiesSpellPower then
+                buffText = string.format("%.1f\n%d", buffExpirationTime, buffData.buffAmount)
+            else
+                buffText = string.format("%.1f", buffExpirationTime)
+            end
+            buffIcon:SetAlpha(1.0)
+            buffIcon.text:SetText(buffText)
+            buffIcon:Show() -- Make sure the buff icon is shown
+
+            -- Set the position of the spell icon based on its index
+            buffIcon:ClearAllPoints()
+            buffIcon:SetPoint("TOPLEFT", buffFrame, "TOPLEFT", (buffIndex-1)*(BUFF_ICON_SIZE + 4), 0)
+
+            buffIndex = buffIndex + 1
+        end
+    end
+
+    -- Hide any inactive buffs that are still in the existingBuffIDs table
+    for spellId, spellIcon in pairs(existingBuffIDs) do
+        if not buffDataContainsSpellId(buffsData, spellId) then
+            ResetInactiveBuffIcon(spellIcon)
+            existingBuffIDs[spellId] = nil
+        end
+    end
+end
+
+local existingDebuffIDs = {}
+
+local function UpdateDebuffDisplay()
+    -- Get the debuff data with spell icons
+    local _, debuffsData = buffsDebuffs()
+
+    -- Handle debuffs
+    local debuffIndex = 1
+    for _, debuffData in pairs(debuffsData) do
+        local debuffIcon = existingDebuffIDs[debuffData.spellId]
+        if not debuffIcon then
+            -- Create a new debuff icon frame
+            debuffIcon = CreateSpellIcon(debuffFrame, debuffData.texture)
+            existingDebuffIDs[debuffData.spellId] = debuffIcon
+        end
+
+        local expirationTime = debuffData.expirationTime - GetTime()
+        if expirationTime <= 0 then
+            ResetInactiveBuffIcon(debuffIcon)
+            existingDebuffIDs[debuffData.spellId] = nil
+        else
+            debuffIcon:SetAlpha(1.0)
+            debuffIcon.text:SetText(format("%.1f", expirationTime))
+            debuffIcon:Show()
+
+            -- Set the position of the spell icon based on its index
+            debuffIcon:ClearAllPoints()
+            debuffIcon:SetPoint("TOPLEFT", debuffFrame, "TOPLEFT", (debuffIndex-1)*DEBUFF_ICON_SIZE, 0)
+
+            debuffIndex = debuffIndex + 1
+        end
+    end
+
+    -- Hide any inactive debuffs that are still in the existingDebuffIDs table
+    for spellId, spellIcon in pairs(existingDebuffIDs) do
+        if not buffDataContainsSpellId(debuffsData, spellId) then
+            ResetInactiveBuffIcon(spellIcon)
+            existingDebuffIDs[spellId] = nil
+        end
+    end
+end
+
+-- Define a function to handle the UNIT_AURA event
+local function OnUnitAuraChanged(self, event, unit)
+    if unit ~= "target" then
+        return
+    end
+
+    -- Update the buff display
+    UpdateBuffDisplay()
+
+    -- Update the rotation display
+    UpdateDebuffDisplay()
+end
+
+-- Register for the UNIT_AURA event to update the buff and rotation displays when the target's auras change
+buffFrame:RegisterEvent("UNIT_AURA")
+buffFrame:SetScript("OnEvent", OnUnitAuraChanged)
+
+debuffFrame:RegisterUnitEvent("UNIT_AURA", "target")
+debuffFrame:SetScript("OnEvent", OnUnitAuraChanged)
+
+-- Create a frame for updating the buffs
+local buffFrameUpdate = CreateFrame("Frame")
+buffFrameUpdate:SetScript("OnUpdate", function(self, elapsed)
+    UpdateBuffDisplay()
+end)
+-- Create a frame for updating the debuffs
+local debuffFrameUpdate = CreateFrame("Frame")
+debuffFrameUpdate:SetScript("OnUpdate", function(self, elapsed)
+    UpdateDebuffDisplay()
+end)
+
+-- Event Handling
+buffFrame:RegisterEvent("ADDON_LOADED")
+buffFrame:RegisterEvent("PLAYER_LOGOUT")
+buffFrame:SetScript("OnEvent", function(self, event, arg1)
+    if event == "ADDON_LOADED" and arg1 == "JescoTest" then
+        InitializePosition(buffFrame)
+    elseif event == "PLAYER_LOGOUT" then
+        SavePosition(buffFrame)
+    end
+end)
---
+++ globals.lua
@@ -0,0 +1,124 @@
+function createFrame(width, height)
+    local frame = CreateFrame("Frame", "frame", UIParent)
+    frame:SetSize(width, height)
+    frame:SetPoint("CENTER")
+    frame:SetMovable(true)
+    frame:EnableMouse(true)
+    frame:RegisterForDrag("LeftButton")
+    frame:SetScript("OnDragStart", function(self) self:StartMoving() end)
+    frame:SetScript("OnDragStop", function(self)
+        self:StopMovingOrSizing()
+        --SavePosition()
+    end)
+    
+    -- Set a background texture
+    local bg = frame:CreateTexture(nil, "BACKGROUND")
+    bg:SetAllPoints()
+    bg:SetColorTexture(0, 0, 0, 0.5)
+    
+    return frame
+end
+
+function SavePosition(frame)
+    local point, _, _, x, y = frame:GetPoint()
+    FrameDB = {point, x, y}
+end
+
+function InitializePosition(frame)
+    local point, x, y
+    if FrameDB then
+        point, x, y = unpack(FrameDB)
+    else
+        point, x, y = "CENTER", 0, 0
+    end
+    frame:SetPoint(point, x, y)
+end
+
+function OnPlayerLogout()
+    SavePosition()
+end
+
+function ResetInactiveBuffIcon(frameIcon)
+    frameIcon:SetAlpha(0.1)
+    frameIcon.text:SetText("")
+    frameIcon:Hide()
+end
+
+function buffDataContainsSpellId(data, spellId)
+    for _, data in pairs(data) do
+        if data.spellId == spellId then
+            return true
+        end
+    end
+    return false
+end
+
+
+function CreateSpellIcon(parent, texture)
+    local iconFrame = CreateFrame("Frame", nil, parent)
+    iconFrame:SetSize(32, 32)
+
+    local iconTexture = iconFrame:CreateTexture(nil, "ARTWORK")
+    iconTexture:SetTexture(texture)
+    iconTexture:SetAllPoints(iconFrame)
+    iconFrame.texture = iconTexture
+
+    local cooldownText = iconFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    cooldownText:SetPoint("BOTTOM", iconFrame, "TOP", 0, -2)
+    cooldownText:SetFont(GameFontNormal:GetFont(), 14)
+    cooldownText:SetTextColor(1, 1, 1)
+    iconFrame.text = cooldownText
+
+    return iconFrame
+end
+
+function buffsDebuffs()
+    local buffsData, debuffsData = {}, {}
+    for i = 1, 40 do -- assuming you want to check all 40 buff/debuff slots
+        local debuffName, _, debuffIcon, _, debuffDuration, debuffExpirationTime, debuffCaster, _, _, debuffSpellId = UnitDebuff("target", i, "HARMFUL|PLAYER")
+        local buffName, _, buffIcon, _, buffDuration, buffExpirationTime, buffCaster, _, _, buffSpellId = UnitBuff("player", i, "HELPFUL|PLAYER")
+        local petBuffName, _, petBuffIcon, _, petBuffDuration, petBuffExpirationTime, petBuffCaster, _, _, petBuffSpellId, _, _,_,_,_,_, buffAmount  = UnitAura("pet", i, "HELPFUL|PLAYER")
+        if debuffName and debuffCaster == "player" then -- if there is a debuff present and it's applied by the player
+            local debuff = {
+                name = debuffName, 
+                icon = debuffIcon, 
+                duration = debuffDuration, 
+                expirationTime = debuffExpirationTime, 
+                texture = GetSpellTexture(debuffSpellId), 
+                spellId = debuffSpellId, 
+                isDebuff = true,
+                modifiesSpellPower = false
+            }
+            table.insert(debuffsData, debuff)
+        end
+        if buffName and buffCaster == "player" then -- if there is a buff present and it's applied by the player
+            local buff = {
+                name = buffName, 
+                icon = buffIcon, 
+                duration = buffDuration, 
+                expirationTime = buffExpirationTime, 
+                texture = GetSpellTexture(buffSpellId), 
+                spellId = buffSpellId, 
+                isDebuff = false,
+                modifiesSpellPower = false
+            }
+            table.insert(buffsData, buff)
+        end
+        if petBuffName and petBuffCaster == "pet" and petBuffName == "Demonic Pact" then
+            local buff = {
+                name = petBuffName,
+                icon = petBuffIcon,
+                duration = petBuffDuration,
+                expirationTime = petBuffExpirationTime,
+                texture = GetSpellTexture(petBuffSpellId),
+                spellId = petBuffSpellId,
+                isDebuff = false,
+                modifiesSpellPower = true,
+                bonus = bonus,
+                buffAmount = buffAmount
+            }
+            table.insert(buffsData, buff)
+        end
+    end
+    return buffsData, debuffsData
+end
